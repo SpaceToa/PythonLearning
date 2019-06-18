6fbb809a2ded6eb27e49fff2a7c9a61b1cd0ce9b
SpaceToa <andye.green15@gmail.com>
2019-06-18 11:08:47 -0400

    Part 4 Done

--- entity.py
+++ entity.py
@@ -2,14 +2,13 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-
-    def __init__(self,x,y,char,color):
+    def __init__(self, x, y, char, color):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
 
     def move(self, dx, dy):
-        #Move the entity by a given amount
+        # Move the entity by a given amount
         self.x += dx
         self.y += dy
